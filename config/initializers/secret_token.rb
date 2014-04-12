572732b3d889f18281bade5a0d8a4d064d3d8dc5
nikolas <nikolasads@gmail.com>
2014-04-12 01:18:10 -0300

    commit dos arquivos do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjetoMiniOpenRedu::Application.config.secret_key_base = '63f3de2b6be0f391aeb083c676e22303abf73995dca821fe2bea609d93069c56ae5386485aa602db14b1f6cd2144da7120c3fb746ee4856569ec00bd256bbc62'
